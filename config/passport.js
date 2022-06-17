e25d8638a5f4a9d81e052e77051c06afd8b5a9a4
MARLIN K MAKORI <marlin.makori@gmail.com>
2022-06-16 22:32:37 -0700

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -42,7 +42,7 @@ passport.use(
         {
             clientID: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
-            callbackURL: "/auth/github/callback",
+            callbackURL: "https://shortstravel.herokuapp.com/auth/github/callback",
         },
         function (accessToken, refreshToken, profile, cb) {
             // console.log(profile)
